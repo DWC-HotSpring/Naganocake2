45156992b512b6d7d0b583bf364810cad66a43c8
vagrant <chorin1911@gmail.com>
2020-08-18 08:47:54 +0000

    product/show

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,5 +3,14 @@ class ProductsController < ApplicationController
   end
 
   def show
+    @product = Product.find(params[:id])
+    @cart = Cart_item.new
   end
+
+  private
+
+    def product_params
+      params.require(:product).permit(:name, :price, :image_id, :description)
+    end
+
 end
