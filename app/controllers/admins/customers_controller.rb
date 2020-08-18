1fa0df9e29e9a98debc2352ee1032036fc1a23bd
Kazuki Kinebuchi <k.k.5.2.26@i.softbank.jp>
2020-08-18 12:49:29 +0000

    ビュー作成

--- app/controllers/admins/customers_controller.rb
+++ app/controllers/admins/customers_controller.rb
@@ -1,10 +1,18 @@
 class Admins::CustomersController < ApplicationController
   def index
+    @customers = Customer.all
   end
 
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def edit
+    @customer = Customer.find(params[:id])
+  end
+
+  private
+  def customer_parmas
+    params.require(:customer).permit(:email, :first_name, :last_name,:first_name_kana, :last_name_kana,:post_code, :prefecture_code, :city, :block, :city, :phone_number)
   end
 end
