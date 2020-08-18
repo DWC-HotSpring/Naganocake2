6547e50132c2c920b48fab173e5627e3aef712c9
vagrant <chorin1911@gmail.com>
2020-08-18 10:05:50 +0000

    products/show/2回目

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,5 +1,11 @@
 class CustomersController < ApplicationController
   def edit
+    @customer = Customer.find(params[:id])
+		if @customer.user.id != current_user.id
+			redirect_to customer_path
+		else
+			render "edit"
+		end
   end
 
   def show
