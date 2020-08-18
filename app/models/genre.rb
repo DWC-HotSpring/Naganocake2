1673f0d007c13df527744dbc63724e6515d0dd67
Kazuki Kinebuchi <k.k.5.2.26@i.softbank.jp>
2020-08-18 07:08:19 +0000

    ジã商品ãã[Update]ビュー修正

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,6 +1,5 @@
 class Genre < ApplicationRecord
     #boolean型はpresence: trueにしてしまうと、falseをblank?メソッドで空だと認識してエラーとなる
     validates :is_active,  inclusion: { in: [true, false]}
-    #enum is_active: {"有効": true, "無効": false}
     has_many :products
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -4,5 +4,4 @@ class Product < ApplicationRecord
 
     #boolean型はpresence: trueにしてしまうと、falseをblank?メソッドで空だと認識してエラーとなる
     validates :is_active,  inclusion: { in: [true, false]}
-    #enum is_active: {"販売中": true, "売り切れ": false}
 end
