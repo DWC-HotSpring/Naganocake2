35ae47525e95ecd79dfe51e94a0d286fb77c7f29
irles24601 <jack_the_fool@outlook.com>
2020-08-20 04:44:54 +0000

    [add] update,destroy,destroy_all action

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -8,15 +8,41 @@ class CartItemsController < ApplicationController
   end
 
   def create
+    @cart_item = current_customer.cart_items.new(cart_item_params)
+    @current_item = CartItem.find_by(product_id: @cart_item.product_id, customer_id: @cart_item.customer_id)
+    if @current_item.nil?
+      if @cart_item.save
+        flash[:success] = 'カートに商品が追加されました！'
+        redirect_to cart_items_path
+      else
+        @carts_items = @customer.cart_items.all
+        render 'index'
+        flash[:danger] = 'カートに商品を追加できませんでした。'
+      end
+    else
+      @current_item.quantity += params[:quantity].to_i
+      @current_item.update(cart_item_params)
+      redirect_to cart_items_path
+    end
   end
 
   def update
+    if @cart_item.update(cart_item_params)
+      flash[:success] = 'カート内の商品を更新しました。'
+      redirect_to cart_items_path
+    end
   end
 
   def destroy
+    @cart_item.destroy
+    flash[:info] = 'カートの商品を取り消しました。'
+    redirect_to cart_items_path
   end
 
   def destroy_all
+    @customer.cart_items.destroy_all
+    flash[:info] = 'カートを空にしました。'
+    redirect_to cart_items_path
   end
 
   private
