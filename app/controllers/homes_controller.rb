ea49e917bbac72d0f0d58d5e0eea161765a98e6c
irles24601 <jack_the_fool@outlook.com>
2020-08-21 08:58:03 +0000

    [add] header homes_controller

---
+++ app/controllers/homes_controller.rb
@@ -0,0 +1,9 @@
+class HomesController < ApplicationController
+  def about
+  end
+
+  def top
+    # 商品の購入回数に応じて上位３位までの商品を降順で取得
+    @rank_items = OrderProduct.find(OrderProduct.group(:product_id).order('sum(quantity) desc').limit(3).pluck(:id))
+  end
+end
