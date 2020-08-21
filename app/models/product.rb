7a8e27a7df512e6306662c37c348174470ec49ea
irles24601 <jack_the_fool@outlook.com>
2020-08-21 12:06:50 +0000

    [add] recent_created? method

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,17 @@
 class Product < ApplicationRecord
+    require "date"
+
     belongs_to :genre
     has_many :cart_items
     has_many :order_products
     has_many :products, through: :order_products
     attachment :image
+
+    #一週間以内に登録されたか判定
+    def recent_created?
+      now = Date.today
+      self.created_at.between?(1.week.ago, now)
+    end
 
     #boolean型はpresence: trueにしてしまうと、falseをblank?メソッドでカラムを空だと認識してエラーとなる
     validates :is_active,  inclusion: { in: [true, false]}
