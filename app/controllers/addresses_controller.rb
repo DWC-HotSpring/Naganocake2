5aff497191720ce69d5b912e9e69d487fe4be84a
vagrant <chorin1911@gmail.com>
2020-08-24 12:14:12 +0000

    配送先

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -2,31 +2,42 @@ class AddressesController < ApplicationController
   
   def index
     @address = Address.new
-    @addresses = Address.all
+    @addresses = current_customer.addresses
   end
 
   def create
-    @address = Address.new
+    @address = Address.new(address_params)
+    @address.customer_id = current_customer.id
+    if @address.save
+      flash[:notice] = "新規登録されました。"
+      redirect_to addresses_path
+    else
+      @addresses = current_customer.addresses
+      render :index
+    end
   end
 
   def edit
     @address = Address.find(params[:id])
-		if @address.customer.id != current_customer.id
-      redirect_to address_path
-      flash[:error] = "error"
+    if @address.customer.id != current_customer.id
+      flash[:notice] = "アクセス権限がありません。"
+      redirect_to addresses_path
 		end
   end
 
   def update
     @address = Address.find(params[:id])
-  	@address.update
- 		redirect_to address_path
+    if @address.update(address_params)
+      redirect_to addresses_path
+    else
+      render :edit
+    end
   end
 
   def destroy
     @address = Address.find(params[:id])
     @address.destroy
-    redirect_to address_path
+    redirect_to addresses_path
   end
 
   private
