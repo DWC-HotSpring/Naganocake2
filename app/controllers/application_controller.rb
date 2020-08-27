eddb2b0ae669d56b2a70a89464c9c059f81b4cfa
Kazuki Kinebuchi <k.k.5.2.26@i.softbank.jp>
2020-08-27 09:53:37 +0000

    [Update]routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
+    #adminログイン後のルーティング 
     def after_sign_in_path_for(resource) 
-        
         if  current_admin
             admins_top_path
         else
