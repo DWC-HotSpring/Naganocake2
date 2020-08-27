8cc7d3fade3995f036c209eaa4d4501cca5fd6e7
irles24601 <jack_the_fool@outlook.com>
2020-08-27 00:18:51 +0000

    [update] header top-view

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -33,6 +33,23 @@ $(function() {
   });
 });
 
+// ヘッダースクロール時の色変化
+$(function(){
+  var imgHeight = $('.js-mainVisual').outerHeight(); //画像の高さを取得。これがイベント発火位置になる。
+  var header = $('.js-header'); //ヘッダーコンテンツ
+
+  $(window).on('load scroll', function(){
+     if ($(window).scrollTop() < imgHeight) {
+       //メインビジュアル内にいるので、クラスを外す。
+       header.removeClass('headerColor-default');
+     }else {
+       //メインビジュアルより下までスクロールしたので、クラスを付けて色を変える
+       header.addClass('headerColor-default');
+     }
+  });
+});
+
+
 // 商品評価
 (function($) {
     'use strict';
