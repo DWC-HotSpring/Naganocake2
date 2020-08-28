8620aac811f32beb9ef9901871f12ee67b6e2f0f
irles24601 <jack_the_fool@outlook.com>
2020-08-28 00:58:41 +0000

    [add] similar products => show page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -49,6 +49,29 @@ $(function(){
   });
 });
 
+// 同じジャンルの商品表示
+$(function(){
+  $('.same-genre').slick({
+    autoplay: true, //自動再生
+    infinite: true, //スライドのループ有効化
+    dots: true, //ドットのナビゲーションを表示
+    slidesToShow: 3, //表示するスライドの数
+    slidesToScroll: 3, //スクロールで切り替わるスライドの数
+    responsive: [{
+      breakpoint: 768, //ブレークポイントが768px
+      settings: {
+        slidesToShow: 2, //表示するスライドの数
+        slidesToScroll: 2, //スクロールで切り替わるスライドの数
+      }
+    }, {
+      breakpoint: 480, //ブレークポイントが480px
+      settings: {
+        slidesToShow: 1, //表示するスライドの数
+        slidesToScroll: 1, //スクロールで切り替わるスライドの数
+      }
+    }]
+  });
+});
 
 // 商品評価
 (function($) {
